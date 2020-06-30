c8c6688f2cfeb0f9a09621be3ecd3868bf4ec2ba
anfractuosity <anfractuosity@anfractuosity.com>
2020-07-01 00:16:36 +0100

    improve ffmpeg quality

--- rainbow_convert_video.py
+++ rainbow_convert_video.py
@@ -9,12 +9,6 @@ WIDTH=2048
 HEIGHT=1536
 scale=2
 
-try:
-    os.remove('out.txt')
-except FileNotFoundError:
-    ok = 1
-
-frames = open('out.txt', 'a')
 img = Image.new('RGB', (WIDTH, HEIGHT), color = 'black')
 draw = ImageDraw.Draw(img)
 
@@ -37,16 +31,13 @@ while(True):
             draw.rectangle(((x+1)*scale ,(y+0)*scale,((x+1)*scale)+scale,((y+0)*scale)+scale), fill=(g,g,g),outline=None,width=0)
             draw.rectangle(((x+0)*scale ,(y+1)*scale,((x+0)*scale)+scale,((y+1)*scale)+scale), fill=(g,g,g),outline=None,width=0)
             draw.rectangle(((x+1)*scale ,(y+1)*scale,((x+1)*scale)+scale,((y+1)*scale)+scale), fill=(r,r,r),outline=None,width=0)
-
-    frames.write("""file frames/%d.png\nduration %f\n""" % (counter,1/24))
+    
     img.save("""frames/%d.png""" % counter)
-
     if counter > 24*1:
         break
 
     counter += 1
 
-frames.close()
 # Should be Python library for this?
-os.system("ffmpeg -f concat -i out.txt -framerate 24 -c:v libx264 out.mkv")
+os.system("ffmpeg -r 23.976 -i frames/%d.png -vcodec libx264 -crf 0 out.mkv")
 
